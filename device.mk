8622182aa017e4bacd1b10f6493c631a8ed97921
Slmie <2258248722@qq.com>
2021-06-08 09:56:55 +0800

    wahoo: VoLTE: Enable Volte
    Signed-off-by: ReallySnow <reallysnow233@gmail.com>
    Change-Id: Ia698198483f1006e8f311f7d84e64bcac8d0ab6b
    Co-Authored-By: 初羽 <reallysnow233@gmail.com>

--- device.mk
+++ device.mk
@@ -750,3 +750,18 @@ include hardware/google/pixel/thermal/device.mk
 
 # power HAL
 -include hardware/google/pixel/power-libperfmgr/hidl/device.mk
+
+# Volte
+# Enable Volte vendor props
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
+    
